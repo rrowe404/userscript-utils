6dfc5cc918ea5a46d791a1563d6609df1f0744f1
Rosalyn Rowe <rrowe404@gmail.com>
2024-07-19 18:06:12 -0700

    this stuff shouldn't have been committed

--- src/leftPad/leftPad.test.js
+++
@@ -1,7 +0,0 @@
-import leftPad from './leftPad';
-describe('leftPad', () => {
-    it('should do the thing', () => {
-        expect(leftPad('hi', 3)).toBe('   hi');
-        expect(leftPad('hello', 8)).toBe('        hello');
-    });
-});

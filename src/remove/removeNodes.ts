a9fa068a3ad3d4ebc6dfa2b20baa41b62d0b8212
Rosalyn Rowe <rrowe404@gmail.com>
2024-07-23 12:28:25 -0700

    add support for single element methods like document.querySelector & methods that return NodeListOf instead of HTMLCollectionOf

--- src/remove/removeNodes.ts
+++ src/remove/removeNodes.ts
@@ -1,10 +1,14 @@
 /**
  * Removes a collection of HTML elements
- * @param nodes result of calling document.getElementsBy(.*) method
+ * @param nodes result of calling document.getElement(.*) or document.querySelector(.*) methods
  * @returns success or failure (no nodes to remove)
  */
-export function removeNodes(nodes: HTMLCollectionOf<Element>): boolean {
-    const arr = [...nodes];
+export function removeNodes(nodes: Element | HTMLCollectionOf<Element> | NodeListOf<Element>): boolean {
+    if (!nodes) {
+        return false;
+    }
+
+    const arr = nodes instanceof Element ? [nodes] : [...nodes];
 
     if (arr.length > 0) {
         arr.forEach(node => node.remove());

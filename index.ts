4f3b809ca4d2e456ee2bfc29b1fc2436c28635f3
Rosalyn Rowe <rrowe404@gmail.com>
2024-11-24 14:30:59 -0800

    remove plugin from here... since it is gonna be used in a webpack config and kinda stands alone from the rest of this stuff, it is now in its own project and uses commonjs for better compatibility

--- index.ts
+++ index.ts
@@ -2,4 +2,3 @@ export { createStylesheet } from "./src/css/createStylesheet";
 export { removeNodes } from "./src/remove/removeNodes";
 export { getNextSibling } from "./src/sibling/getNextSibling";
 export { getPreviousSibling } from "./src/sibling/getPreviousSibling";
-export { UserscriptPlugin } from "./src/plugin/UserscriptPlugin";

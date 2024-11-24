52e809efeccf5adabdcbc6e09eccefc458f946b7
Rosalyn Rowe <rrowe404@gmail.com>
2024-11-23 23:53:57 -0800

    lets make a plugin

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export { createStylesheet } from "./src/css/createStylesheet";
 export { removeNodes } from "./src/remove/removeNodes";
 export { getNextSibling } from "./src/sibling/getNextSibling";
 export { getPreviousSibling } from "./src/sibling/getPreviousSibling";
+export { UserscriptPlugin } from "./src/plugin/UserscriptPlugin";
